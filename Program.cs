9ae31813cc0639473b96684b626d96efc4af401d
vladislav-magnificent <vladasusrog1love@gmail.com>
2020-12-10 22:58:20 +0300

    module 11.3

--- Program.cs
+++ Program.cs
@@ -6,7 +6,18 @@ namespace FirstApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string MyName = "Jane";
+            byte MyAge = 27;
+            bool HaveIPet = true;
+            double MyShoeSize = 37.5;
+
+            Console.WriteLine("My Name is " + MyName);
+            Console.WriteLine("My Age is " + MyAge);
+            Console.WriteLine("Do I have a pet " + HaveIPet);
+            Console.WriteLine("My shoe size is " + MyShoeSize);
+
+            Console.ReadKey();
+
         }
     }
 }
